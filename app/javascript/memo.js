4758d5fd37d3c4a9b00dc1373bb3cf364c13a2a0
glgsysyoshimoto <glgsysyoshimoto@gmail.com>
2026-01-16 20:20:41 +0900

    JSでレスポンスを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,3 +1,17 @@
+const buildHTML = (XHR) => {
+  const item = XHR.response.post;
+  const html = `
+    <div class="post">
+      <div class="post-date">
+        投稿日時：${item.created_at}
+      </div>
+      <div class="post-content">
+        ${item.content}
+      </div>
+    </div>`;
+  return html;
+};
+
 function post (){
   //  console.log("イベント発火");
   const form = document.getElementById("form");
@@ -6,9 +20,34 @@ function post (){
     // console.log("イベント発火");
     const formData = new FormData(form);
     const XHR = new XMLHttpRequest();
-    XHR.open("POST", "/posts", true);
+    XHR.open("POST", "/posts", true);  //「post」→「posts」に戻す
+    // XHR.open("POST", "/post", true);  //「posts」→「post」にする
+    // XHR.open("POST", "/posts", true);
     XHR.responseType = "json";
     XHR.send(formData);
+    XHR.onload = () => {
+      if (XHR.status != 200) {
+        alert(`Error ${XHR.status}: ${XHR.statusText}`);
+        return null;
+      };
+      // console.log(XHR.response);
+      const list = document.getElementById("list"); 
+      const formText = document.getElementById("content");
+      // console.log(formText.value);
+      const item = XHR.response.post;
+      const html = `
+        <div class="post">
+          <div class="post-date">
+            投稿日時：${item.created_at}
+          </div>
+          <div class="post-content">
+            ${item.content}
+          </div>
+        </div>`;
+      // list.insertAdjacentHTML("afterend", html);
+      list.insertAdjacentHTML("afterend", buildHTML(XHR));
+      formText.value = "";
+    };
   });
 };
 
